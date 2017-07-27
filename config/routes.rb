9561f0ad0f567cc6c0de427fea84ceb78c485739
Adithya Iyengar <adithya_iyengar@hotmail.com>
2017-07-27 13:17:04 -0400

    created mailer for contact page, flash messages, and added form field for sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'about', to: 'pages#about'
-  resources :contacts
+  resources :contacts, only: :create
+  get 'contact-us', to: 'contacts#new', as: 'new_contact'
   get '/hot_rally_city', to: 'pages#city'
 end
