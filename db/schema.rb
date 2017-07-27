115eb999a48250c7daab31331b01544e668c951a
Adithya Iyengar <adithya_iyengar@hotmail.com>
2017-07-26 21:54:58 -0400

    added contacs page in navbar, added routes, and controller, created contacts database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170725182735) do
+ActiveRecord::Schema.define(version: 20170727015258) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "subject"
+    t.text "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.float "latitude"
