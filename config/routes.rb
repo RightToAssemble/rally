0c41021f457642bd2ad6acf9b00d84121efff278
Adithya Iyengar <adithya_iyengar@hotmail.com>
2017-07-27 11:06:13 -0400

    fixed merging errors that werent fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-=======
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
->>>>>>> forkotis/rally_creation
+  devise_for :users
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   resources :contacts
