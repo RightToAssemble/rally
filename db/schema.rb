19952fbe76e48f56f63c3b961a8a0ea977af6c25
Adithya Iyengar <adithya_iyengar@hotmail.com>
2017-08-05 11:34:33 -0400

    what am I doing

--- db/schema.rb
+++ db/schema.rb
@@ -25,12 +25,14 @@ ActiveRecord::Schema.define(version: 20170804185439) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "new_rallies", force: :cascade do |t|
+  create_table "newrallies", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.string "organization"
     t.string "city"
     t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "profiles", force: :cascade do |t|
